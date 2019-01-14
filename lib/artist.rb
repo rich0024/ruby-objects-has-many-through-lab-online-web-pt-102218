ef721468705ec1f2be1721f97d7b05a84c6a165c
Richard Yance <yance0022@gmail.com>
2019-01-14 23:34:00 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+class Artist
+
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+    @songs = []
+  end
+
+  def all
+    @@all
+  end
+
+  def new_song(name, genre)
+    song = Song.new(name, self, genre)
+    @songs << song
+    song
+  end
+
+  def songs
+    @songs
+  end
+  
+end
---
+++ lib/song.rb
@@ -0,0 +1,9 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name)
+    @name = name
+  end
+
+end
